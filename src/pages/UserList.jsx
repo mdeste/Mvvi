5b53a0cca8097a252b02ad0f1eab6859612ec71c
Michael Deste <michaeldeste@gmail.com>
2022-04-20 15:15:45 +0100

    added loggedin check for watchlist

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,11 +1,34 @@
 import {Link} from 'react-router-dom'
+import {useAuthStatus} from '../hooks/useAuthStatus'
+import Spinner from '../components/Spinner'
 
 function UserList() {
-  return (
+  const {loggedIn, checkingStatus} = useAuthStatus()
+
+  if(checkingStatus) {
+    return <Spinner />
+  }
+
+  if(!loggedIn) {
+    return (
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">WATCHLIST</p>
+        </header>
+        <main className="mainDivAboutContent">
+          <p className="aboutPageContent">
+            You are not logged in. To view and add to your watchlist, please log in or create an account.
+          </p>
+        </main>
+      </div>
+    )
+  } else return (
     <div className="pageContainer">
       <header>
         <p className="pageHeader">WATCHLIST</p>
       </header>
+
+      
 
       <main className="mainDivAboutContent">
         <div className="homepageBar">
