f88da120378159c8aa9efed1561bb04aeae709a4
Michael Deste <michaeldeste@gmail.com>
2022-05-01 15:22:38 +0100

    rendering first item from listArray in DOM

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,13 +1,16 @@
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, useNavigate, useParams} from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import {useAuthStatus} from '../hooks/useAuthStatus'
-import {updateDoc, doc, collection, getDocs, query, where, orderBy} from 'firebase/firestore'
+import {updateDoc, doc, collection, getDoc, getDocs, query, where, orderBy} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
+import heartIcon from '../assets/svg/heartIcon.svg'
+
 
 function UserList() {
   const [loading, setLoading] = useState(true)
   const [listArray, setListArray] = useState(null)
+  const [listItem, setListItem] = useState(null)
   
   const {loggedIn, checkingStatus, uid} = useAuthStatus()
 
@@ -29,7 +32,8 @@ function UserList() {
             data: doc.data()
           })
         })
-
+        
+        setListItem(listArray.original_title)
         setListArray(listArray)
         setLoading(false)
     }
@@ -60,7 +64,31 @@ function UserList() {
         <p className="pageHeader">WATCHLIST</p>
       </header>
 
-      
+      {/* <p>{listArray[0].data.id}</p> */}
+
+      {!loading && listArray?.length > 0 && (
+        <>
+          <ul className="movieList">
+          <main className="mainResult">
+        <div className="searchResultContainer">
+            <div className="searchResultTitleLink">
+                <Link className="searchResultLink" to={`/movie/${listArray[0].data.id}`}>{listArray[0].data.original_title}</Link>
+            </div>
+            <img src={heartIcon} alt="Heart Icon" className="heartIcon" />
+            <div className="searchResultPosterContainer">
+                <img src={`${process.env.REACT_APP_TMDB_IMGURL}${listArray[0].data.poster_path}`} alt="Movie Poster" className="searchResultPoster"/>
+            </div>
+            <div className="searchResultReleaseDate">
+                Release year: {listArray[0].data.release_date.slice(0, -6)}
+            </div>
+            <div className="searchResultVoteAverage">
+                Average rating: {listArray[0].data.vote_average}/10
+            </div>
+        </div>
+    </main>
+          </ul>
+        </>
+      )}
 
       <main className="mainDivAboutContent">
         <div className="homepageBar">
